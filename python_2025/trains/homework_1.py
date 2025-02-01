00a176b9197b0d92ead9125a59684928d32adf95
Piotr Marecki <pmarecki@gmail.com>
2025-02-01 15:16:03 +0100

    version of destination sorting via reverse=True on source sorting

--- python_2025/trains/homework_1.py
+++ python_2025/trains/homework_1.py
@@ -4,13 +4,30 @@ from sys import thread_info
 from python_2025.trains.importer import TrainInfo
 
 
-def get_trains_from(city_prefix: str, all_trains: list[TrainInfo]) -> list[TrainInfo]:
+def reverse_train(train: TrainInfo) -> TrainInfo:
+    return TrainInfo(
+        id=train.id,
+        train_name=train.train_name,
+        train_number=train.train_number,
+        source=train.destination,
+        destination=train.source,
+        distance=train.distance,
+        total_time_minutes=train.total_time_minutes,
+        departure_time=train.arrival_time,
+        arrival_time=train.departure_time,
+    )
+
+
+def get_trains_from(city_prefix: str, all_trains: list[TrainInfo], reverse=False) -> list[TrainInfo]:
     if len(city_prefix) == 0:
         return []
     res = []
     city_prefix = city_prefix.lower()
     for t in all_trains:
-        if t.source.lower().startswith(city_prefix):
+        g = t
+        if reverse:
+            g = reverse_train(t)
+        if g.source.lower().startswith(city_prefix):
             res.append(t)
     return res
 
@@ -89,6 +106,18 @@ def test_get_trains_diacritics_normalization():
 
     assert len(get_trains_from('rzeszów', [t1, t2])) == 2
     assert len(get_trains_from('RZESZÓW', [t1, t2])) == 2
+
+
+def test_get_trains_on_destination():
+    t1 = TrainInfo(id=1, train_name='a', train_number='01', source='Bielsko-Biała', destination='Warszawa', distance=250,
+                   total_time_minutes=240, departure_time=time(12, 0, 0), arrival_time=time(15, 0, 0))
+    t2 = TrainInfo(id=2, train_name='b', train_number='01', source='Wrocław', destination='Warszawa', distance=250,
+                   total_time_minutes=240, departure_time=time(12, 0, 0), arrival_time=time(15, 0, 0))
+    t3 = TrainInfo(id=3, train_name='c', train_number='01', source='Gdańsk', destination='Warszawa', distance=250,
+                   total_time_minutes=240, departure_time=time(12, 0, 0), arrival_time=time(15, 0, 0))
+
+    res = get_trains_from('War', [t1, t2, t3], reverse=True)
+    print(res)
 
 
 if __name__ == '__main__':
