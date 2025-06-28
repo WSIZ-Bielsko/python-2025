d3557e7b9cc1e2922d19ea7aa65f7e10c1d78f2f
Piotr Marecki <pmarecki@gmail.com>
2025-06-28 14:50:22 +0200

    add some ideas how to construct the full game

--- python_2025/prisoners_dilemma/a.py
+++ python_2025/prisoners_dilemma/a.py
@@ -6,6 +6,28 @@ class Score(BaseModel):
     player2: float
 
 
+class Game:
+    def __init__(self, payoff: dict[str, tuple[float, float]]):
+        self.payoff = payoff
+
+    def post_player1_move(self, move: str):
+        # record move; if both players have made a move, compute score and update scores
+        # and reset moves
+        pass
+
+    def post_player2_move(self, move: str):
+        pass
+
+    def _update_scores(self):
+        pass
+
+    def reset_game(self):
+        pass
+
+    def get_scores(self) -> Score:
+        pass
+
+
 def compute_updated_score(decisions: str, current_score: Score, payoff: dict[str, tuple[float, float]]) -> Score:
     """
     Assume decisions are keys in payoff dict.
@@ -43,6 +65,8 @@ def test_update_score2():
 
 if __name__ == '__main__':
     payoff = {'DD': (0, 0.5), 'CD': (-1, 2), 'DC': (1, -1), 'CC': (0.5, 1)}
+
+    print(payoff['DD'])  # (0,0.5)
 
     scores = Score(player1=0, player2=0)  # tworzymy instancje klasy
     print(scores)
